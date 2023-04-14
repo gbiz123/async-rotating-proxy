71de4b201f46fadaaef1298fe3bcd6292ce2dc1f
greg <g.bizup@gmail.com>
2023-04-14 14:23:28 -0700

    check

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rotating_proxy_api",
+    name="async_rotating_proxy",
     version="0.1",
     license="MIT",
     author="Gregory Bizup",
     author_email = "g.bizup@gmail.com",
-    packages=find_packages("rotating_proxy_api"),
-    pacakge_dir={'': 'proxy_api'},
-    url="https://github.com/gbiz123/rotating-proxy-api",
+    packages=find_packages("async_rotating_proxy"),
+    pacakge_dir={'': 'async_rotating_proxy'},
+    url="https://github.com/gbiz123/async-rotating-proxy",
     keywords=["proxy", "rotating proxy", "pyppeteer proxy", "selenium proxy", "scraping"],
     install_requires=[
         "aiohttp==3.8.4",
